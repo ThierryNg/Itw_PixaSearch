37a9109cafc8900a71db156c08a1f76d779a8530
Thierry Ng <ng.thierry.d@gmail.com>
2023-04-17 21:50:12 +0900

    adding Image model, request sub-class

---
+++ PixaSearch/PixaSearch/Network/ImageResponse.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  PixaSearch
+//
+//  Created by Thierry on 2023/04/17.
+//
+
+import Foundation
---
+++ PixaSearch/PixaSearch/Shared models/PixaImage.swift
@@ -0,0 +1,8 @@
+//
+//  PixaImage.swift
+//  PixaSearch
+//
+//  Created by Thierry on 2023/04/17.
+//
+
+import Foundation
